1d7985ccc8daa86e4a5aa3100ff2090ecbb02556
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2025-01-03 22:51:35 +0700

    fix(title): Fix title app name

--- resources/views/layouts/main/master.blade.php
+++ resources/views/layouts/main/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Hope UI | Responsive Bootstrap 5 Admin Dashboard Template</title>
+    <title>{{ $title ?? config('app.name') }}</title>
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
